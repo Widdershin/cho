0ea79d7b501ec4cc3a615eb349dc17c32c048847
Nick Johnstone <ncwjohnstone@gmail.com>
2014-11-27 17:02:16 +1300

    Receiving json

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 io.on('connection', function (socket) {
   console.log('someone connected');
@@ -15,6 +17,11 @@ io.on('connection', function (socket) {
 
 app.get('/', function (request, response) {
   response.sendFile('index.html', { root: __dirname });
+});
+
+app.post('/scores/', function (request, response) {
+  var player = request.body.player;
+  response.send(200);
 });
 
 http.listen(3000, function() {
